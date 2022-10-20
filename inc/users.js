b5ce95fcf1740859a3968dabfb653ee144f64ffc
zYumoto <victorjuanrazer@gmail.com>
2022-10-20 16:21:16 -0300

    add: router-level middleware to validate login session

---
+++ inc/users.js
@@ -0,0 +1,46 @@
+var conn = require("./db")
+
+module.exports = {
+
+    render(req, res, error) {
+
+        res.render("admin/login", {
+            body: req.body,
+            error
+        });
+
+    },
+
+    login(email, password) {
+
+        return new Promise((resolve, reject) => {
+            conn.query(`
+            SELECT * FROM tb_users WHERE email = ?`,
+                [
+                    email
+                ], (err, results) => {
+
+                    if (err)
+                        reject(err);
+
+                    else {
+
+                        if (!results.length > 0)
+                            reject('Usuarios ou senha invalidos');
+
+                        else {
+
+                            let row = results[0];
+
+                            if (row.password !== password)
+                                reject('Usuarios ou senha invalidos');
+
+                            else {
+                                resolve(row);
+                            }
+                        }
+                    }
+                })
+        })
+    }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,42 +1,47 @@
 var express = require("express")
+var users = require('./../inc/users')
 var router = express.Router();
 
 
 router.get("/", function (req, res, next) {
-
     res.render("admin/index");
 });
 
-router.get("/login", function (req, res, next) {
+router.post("/login", function (req, res, next) {
 
-    res.render("admin/login");
+    if (!req.body.email) {
+        users.render(req, res, "Preencha o campo de email !!")
+    } else if (!req.body.password) {
+        users.render(req, res, "Preencha o campo senha !!")
+    } else {
+
+        users.login(req.body.email, req.body.password).then(user => {
+            req.session.user = user;
+            res.redirect("/admin");}).catch(err => {
+    users.render(req, res, err.message || err)
+})
+}
+});
+
+router.get("/login", function (req, res, next) {
+    users.render(req, res, null)
 });
 
 router.get("/contacts", function (req, res, next) {
-
     res.render("admin/contacts");
 });
-
 router.get("/emails", function (req, res, next) {
-
     res.render("admin/emails");
 });
-
 router.get("/menus", function (req, res, next) {
-
     res.render("admin/menus");
 });
-
 router.get("/reservations", function (req, res, next) {
-
     res.render("admin/reservations", {
         date: {}
     });
 });
-
 router.get("/users", function (req, res, next) {
-
     res.render("admin/users");
 });
-
 module.exports = router
