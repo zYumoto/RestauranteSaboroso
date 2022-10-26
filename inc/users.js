c99d5cd0459d5b419e2bff702b6c52aea382f1af
zYumoto <victorjuanrazer@gmail.com>
2022-10-26 13:29:25 -0300

    add:User administration creating routes

--- inc/users.js
+++ inc/users.js
@@ -39,4 +39,82 @@ module.exports = {
             
         });
     },
+
+    
+    getUsers() {
+
+
+    },
+
+
+    getUsers() {
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(`
+            SELECT * FROM tb_users ORDER BY name`,
+                (err, results) => {
+
+                    if (err)
+                        reject(err);
+                    else
+                        resolve(results);
+                })
+
+        });
+    },
+
+    save(fields, files) {
+
+        return new Promise((resolve, reject) => {
+
+            let query, params = [
+                fields.name,
+                fields.email,
+            ];
+
+            if (parseInt(fields.id) > 0) {
+
+                params.push(fields.id)
+
+                query = `
+                    UPDATE tb_users
+                    SET name = ?,
+                        email = ?
+                    WHERE id = ? `;
+            } else {
+                query = `
+                INSERT INTO tb_users (name, email, password)
+                VALUES (?, ?, ?)`;
+
+                params.push(fields.password)
+            }
+
+            conn.query(query, params, (err, results) => {
+                console.log(err);
+                if (err)
+                    reject(err);
+                else
+                    resolve(results);
+            });
+        });
+    },
+
+    delete(id) {
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(`
+            DELETE FROM tb_users WHERE id = ?`, [
+                id
+            ], (err, results) => {
+
+                if (err)
+                    reject(err);
+                else
+                    resolve(results);
+            })
+        })
+    }
+    
 };
--- public/js/hcode-gride.js
+++ public/js/hcode-gride.js
@@ -31,6 +31,11 @@ class HcodeGrid {
             formUpdate: '#modal-update form',
             btnUpdate: '.btn-update',
             btnDelete: '.btn-delete',
+            onUpdateLoad: (form, name, data) => {
+
+                let input = form.querySelector('[name=' + name + ']')
+                if (input) input.value = data[name];
+            }
         }, configs);
 
 
